cbaa36c3f0e0fbe542c360f9e58e01573cc80d54
Ankush-Pathak <ankush.pathak151196@gmail.com>
2016-07-16 13:19:45 +0530

    Added button in MainActivity

---
+++ app/src/main/java/ml/alohomora/mmdms/CustomListAdapter.java
@@ -0,0 +1,10 @@
+package ml.alohomora.mmdms;
+
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+
+/**
+ * Created by Ankush on 7/16/2016.
+ */
+public class CustomListAdapter extends ArrayAdapter {
+}
--- app/src/main/java/ml/alohomora/mmdms/MainActivity.java
+++ app/src/main/java/ml/alohomora/mmdms/MainActivity.java
@@ -1,13 +1,16 @@
 package ml.alohomora.mmdms;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,15 +18,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Button button = (Button)findViewById(R.id.buttonLaunchTabbed);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(MainActivity.this,PrimaryTabbedActivity.class);
+                startActivity(intent);
+                finish();
             }
         });
     }
